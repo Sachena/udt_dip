f143367cb783cf4ec7bc800920e464a05de62f7a
Sachena <1217smj@naver.com>
2023-07-05 16:45:01 +0900

    first commit

---
+++ src/test/java/com/example/udt_dip/UdtDipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udt_dip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdtDipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
