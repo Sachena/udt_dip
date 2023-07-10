96dc6c08e973c71a20ab7c39c6e619a95dd85f1f
pureliture <tkdgur1756@naver.com>
2023-07-10 16:47:10 +0900

    [feat] hibernate, h2 관련 설정 추가, sample data dml 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO mobile_phone (MANUFACTURER, MODEL, NAME, USIM, DESCRIPTION, PRICE) VALUES ('SAMSUNG', 'SM-F926N512', '갤럭시 Z 폴드3 512G', 'NANO', '', 2097700);
+INSERT INTO mobile_phone (MANUFACTURER, MODEL, NAME, USIM, DESCRIPTION, PRICE) VALUES ('SAMSUNG', 'SM-F926N', '갤럭시 Z 폴드3', 'NANO', '', 1998700);
+INSERT INTO mobile_plan (name, price, description) values ('5G 프리미어 에센셜', '85000', '');
+INSERT INTO mobile_plan (name, price, description) values ('5G 스탠다드', '75000', '');
+INSERT INTO customer (name, resident_number) values ('테스터', '123456-1234567');
+INSERT INTO contract (phone_number, first_contract_date, customer_id, mobile_phone_id, mobile_plan_id, contract_change_datetime, final_communication_expense) values ('010-1234-5678', '2023-07-10 15:04:00.0', 1, 1, 1, '2023-07-10 15:04:00.0', 85000);
